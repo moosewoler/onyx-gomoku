1a983866096d400574c4b94e7c019baf3a62da8f
Moose W. Oler <wangjun.cumtb@ieee.org>
2013-02-03 13:57:55 +0800

    init commit. remove status_bar_ related codes.

---
+++ src/main.cpp
@@ -0,0 +1,29 @@
+#include <QApplication>
+#include "gomoku_widget.h"
+#include "main_widget.h"
+
+#include <QLocale>
+#include <QTranslator>
+#include "onyx/base/base.h"
+
+#include "onyx/screen/screen_proxy.h"
+#include "onyx/screen/screen_update_watcher.h"
+#include "onyx/sys/sys_status.h"
+#include "onyx/ui/languages.h"
+
+int main(int argc, char **argv)
+{
+    QApplication app(argc, argv);
+    app.setApplicationName("gomoku");
+    QTranslator translator;
+    translator.load(":/gomoku_" + QLocale::system().name());
+    app.installTranslator(&translator);
+    ui::loadTranslator (QLocale::system().name());
+
+    sys::SysStatus::instance().setSystemBusy(false);
+    MainWidget widget;
+
+    widget.showFullScreen();
+    onyx::screen::instance().flush(0, onyx::screen::ScreenProxy::GC);
+    return app.exec();
+}
